2172f63e942d37c5f163bfdb39767cbab2a7d736
UzDevid <uzdevid@gmail.com>
2023-04-20 11:57:15 +0500

    Доработка

--- widgets/Notification/Notification.php
+++ widgets/Notification/Notification.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace uzdevid\dashboard\notifications\widgets;
+namespace uzdevid\dashboard\notification\widgets\Notification;
 
+use Yii;
 use yii\base\Widget;
 
 class Notification extends Widget {
@@ -11,7 +12,7 @@ class Notification extends Widget {
     public function run(): string {
         $user = Yii::$app->user->identity;
 
-        $unread_notifications = \uzdevid\dashboard\models\Notification::find()->where(['user_id' => $user->id])->andWhere(['is_read' => 0])->all();
+        $unread_notifications = \uzdevid\dashboard\notification\models\Notification::find()->where(['user_id' => $user->id])->andWhere(['is_read' => 0])->all();
         return $this->render('index', compact('unread_notifications'));
     }
 }
