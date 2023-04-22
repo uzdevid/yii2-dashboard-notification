a4926e713826a487f675162acc515651f2a65d61
UzDevid <uzdevid@gmail.com>
2023-04-22 16:23:23 +0500

    При клике на колокольчик, обновляет список уведомлении.

--- widgets/Notification/views/index.php
+++ widgets/Notification/views/index.php
@@ -1,5 +1,5 @@
 <li class="nav-item dropdown">
-    <a class="nav-link nav-icon btn btn-outline-light" data-bs-toggle="dropdown">
+    <a onclick="getNotificationsMiniList()" class="nav-link nav-icon btn btn-outline-light" data-bs-toggle="dropdown">
         <i class="bi bi-bell"></i>
         <span id="notifications-badge" class="badge bg-primary badge-number" style="<?php echo count($unread_notifications) == 0 ? 'display:none;' : ''; ?>"><?php echo count($unread_notifications); ?></span>
     </a>
